409581882b52ac801684da390d44fecd5d2b815a
RDC | Desenvolvimento <102391413+mirvoxtm@users.noreply.github.com>
2024-03-20 20:13:39 -0300

    Update main2.hs

--- main2.hs
+++ main2.hs
@@ -1,3 +1,5 @@
 -- Bruxaria
 data Pessoa = Fisica{nome::String, cpf::String} 
               | Juridica{razao::String, cnpj::String} deriving Show
+
+--https://prod.liveshare.vsengsaas.visualstudio.com/join?DF05CF11BCFCF0E1B88EAF22B2351DE43467
